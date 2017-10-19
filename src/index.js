0cf8b0e1f1a22a1f8bfc45838643979836660a2b
Congwen Ma <lostaznfound@gmail.com>
2017-10-19 01:42:58 -0500

    added support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import todoStore from './todoStore'
+import TodoList from './TodoList'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import 'basscss'
+
+
+ReactDOM.render(
+  <TodoList store={ todoStore } />,
+  document.getElementById('root')
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
